b979f4de4dc12e4e6a4dfbe9613f4a4e00954439
Fernando L. Albert <fernandoalbert19@gmail.com>
2025-07-23 15:54:31 -0400

    server updated const fs

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
+const fs = require('fs');
 const connectDB = require('./config/db');
+ // <-- Add this
+
 
 dotenv.config();
 connectDB();
